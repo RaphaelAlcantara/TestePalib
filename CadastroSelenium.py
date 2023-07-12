d6705a8d22569528ea7178a73c440f45e42baa90
RaphaelAlcantara <rvfra@discente.ifpe.edu.br>
2023-07-12 15:15:01 -0300

    teste login invalido

--- CadastroSelenium.py
+++ CadastroSelenium.py
@@ -1,10 +1,10 @@
+import logging
+import time
+
 import pyautogui as pyautogui
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.remote.webelement import WebElement
 
 import geradorPessoas as Gp
-import time
-import logging
 
 # Configurar o logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -111,114 +111,81 @@ try:
     navegador.find_element('xpath', '//*[@id="formCadUsuario:j_idt41"]').click()
     time.sleep(1)
     # #usar lib BY
-    mensagem = navegador.find_element(By.XPATH, '/html/body/div[5]/span[3]').text
+    mensagemCadastroSucesso = navegador.find_element(By.XPATH, '/html/body/div[5]/span[3]').text
 
-    assert mensagem == "Usuario cadastrado com sucesso!"
+    assert mensagemCadastroSucesso == "Usuario cadastrado com sucesso!"
     logging.info('Teste de cadastro de usuário com dados válidos finalizado com sucesso')
     num_tests_passed += 1
 
     logging.info('-' * 50)
+    # -------------------------------------------------------------------------------------------------------------
+
+    # Teste 2 de login com dados inválidos
+    logging.info('Iniciando teste de login com dados inválidos')
+
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').click()
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').send_keys(cpf)
+    time.sleep(0.5)
+
+    # preenche a senha
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').click()
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').clear()
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').send_keys('senhaRuimGG')
+
+    time.sleep(0.5)
+
+    navegador.find_element('xpath', '//*[@id="formLogin:j_idt18"]').click()
+    time.sleep(0.5)
+
+    LoginInvalido = navegador.find_element(By.CSS_SELECTOR, '.col-xs-11 > span:nth-child(4)').text
+    print(LoginInvalido)
+    time.sleep(0.5)
+    assert LoginInvalido in "Login ou Senha não conferem"
+
+    logging.info('Teste de login com dados inválidos finalizado com sucesso')
+
+    num_tests_passed += 1
+
+    logging.info('-' * 50)
+
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').clear()
+    time.sleep(0.5)
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').clear()
+
+    # -------------------------------------------------------------------------------------------------------------
 
     # Teste de login com sucesso
     logging.info('Iniciando teste de login com dados válidos')
+    #url igual a inicial
+    navegador.get("http://localhost:8080/NovoPalib/")
 
     navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').click()
+    navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').clear()
+    time.sleep(0.5)
     navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt12"]').send_keys(cpf)
     time.sleep(0.5)
 
     #preenche a senha
     navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').click()
     navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').clear()
+    time.sleep(0.5)
     navegador.find_element('xpath', '//*[@id="input_formLogin:j_idt14"]').send_keys(senha)
 
     time.sleep(0.5)
 
     navegador.find_element('xpath', '//*[@id="formLogin:j_idt18"]').click()
 
-    msgloginNome = navegador.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/b[1]').text
-    msgloginCpf = navegador.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/b[2]').text
+    msgloginNome = navegador.find_element(By.XPATH, '//*[@id="dtLformMenuCliente:j_idt15"]').text
 
-    assert msgloginNome == nome
-    assert msgloginCpf == cpf
+    print(msgloginNome)
+    assert msgloginNome == "Cliente: " + nome
+
 
     logging.info('Teste de login com dados válidos finalizado com sucesso')
 
     num_tests_passed += 1
 
     logging.info('-' * 50)
-
-    # Teste de adição de livros
-    logging.info('Iniciando teste de adição de livros')
-    #Entrou no sistema admin
-    navegador.find_element('xpath', '/html/body/div[1]/div/div[2]/a[3]').click()
-    navegador.find_element('xpath', '//*[@id="formIndexLivro:j_idt7"]').click()
-    #Nome do livro
-    nomeLivro = 'Livro de teste'
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt10"]').click()
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt10"]').send_keys(nomeLivro)
-    #Autor do livro
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt12"]').click()
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt12"]').send_keys('Autor de teste')
-    #Sinopse do livro
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt14"]').click()
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt14"]').send_keys('sinopse de teste')
-    time.sleep(1)
-
-    #Valor do livro
-    navegador.find_element('xpath', '//*[@id="formCadLivro:TabDadosLivro:j_idt16_input"]').click()
-    navegador.find_element('xpath', '//*[@id="formCadLivro:TabDadosLivro:j_idt16_input"]').send_keys(Keys.BACK_SPACE * 4)
-    time.sleep(1)
-    navegador.find_element('xpath', '//*[@id="formCadLivro:TabDadosLivro:j_idt16_input"]').send_keys(
-        "4,24")
-
-
-    #Categoria do livro
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt18"]').click()
-    navegador.find_element('xpath', '//*[@id="input_formCadLivro:TabDadosLivro:j_idt18"]').send_keys('categoria de teste')
-
-    #clica no botão proximo
-    navegador.find_element('xpath', '//*[@id="formCadLivro:TabDadosLivro:j_idt24"]').click()
-    time.sleep(1)
-
-    #fazendo upload da imagem
-
-    upload_img = navegador.find_element('xpath', '//*[@id="formCadLivro:TabUploadsLivro:fotoUploader_label"]')
-    upload_img.click()
-    time.sleep(1)
-    pyautogui.write('G:\ProjetosPython\pythonProject\pythonProject\TestePalib\imgTeste.jpg')
-    time.sleep(0.5)
-    pyautogui.press('enter')
-    pyautogui.press('enter')
-    time.sleep(1)
-    navegador.find_element('xpath', '/html/body/div[2]/div/div/div[2]/form/div/div/div[2]/div/div/div/div/div[1]/div[1]/button[1]/span[2]').click()
-
-    assert navegador.find_element('xpath', '/html/body/div[2]/div/div/div[2]/form/div/div/div[2]/div/div/div/div/h6').text == 'Foto Adicionada'
-
-
-    #upload do arquivo pdf
-    upload_pdf = navegador.find_element('xpath', '//*[@id="formCadLivro:TabUploadsLivro:pdfUploader_label"]')
-    upload_pdf.click()
-    time.sleep(1)
-    pyautogui.write('G:\ProjetosPython\pythonProject\pythonProject\TestePalib\pdfTeste.pdf')
-    time.sleep(0.5)
-    pyautogui.press('enter')
-    pyautogui.press('enter')
-    time.sleep(1)
-    navegador.find_element('xpath', '/html/body/div[2]/div/div/div[2]/form/div/div/div[2]/div/div/div/div/div[2]/div[1]/button[1]/span[2]').click()
-    assert navegador.find_element('xpath', '/html/body/div[2]/div/div/div[2]/form/div/div/div[2]/div/div/div/div/h6[2]').text == 'Upload de PDF concluido'
-
-    #clica no botão inserir
-    navegador.find_element('xpath', '//*[@id="formCadLivro:TabUploadsLivro:j_idt28"]').click()
-    time.sleep(1)
-
-    #verificar se o texto está na página
-    assert navegador.find_element('xpath', "//*[contains(text(),'Livro de teste')]")
-    num_tests_passed += 1
-
-    logging.info('Teste de adição de livros finalizado com sucesso')
-    logging.info('-' * 50)
-
-    navegador.find_element('xpath', '/html/body/div[1]/div/a').click()
 
 
 
